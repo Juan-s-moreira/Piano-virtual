aff96a2d30a9ef60f7c9e73054cbc952a33afea6
Juan Santos <juan.moreira62@outlook.com>
2024-10-24 13:56:18 -0300

    finalizando projeto

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,4 +1,6 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const volumeSlider = document.querySelector(".volume-slider input")
+const keysCheck = document.querySelector(".keys-check input")
 
 
 let mapedKeys = [];
@@ -27,4 +29,16 @@ document.addEventListener("keydown", (e) => {
     if (mapedKeys.includes(e.key)) {
         playTune(e.key)
     }
-})
+});
+
+const handleVolume = (e) => {
+    audio.volume = e.target.value;
+}
+
+const showHideKeys = () => {
+    pianoKeys.forEach(key => key.classList.toggle("hide"))
+}
+
+volumeSlider.addEventListener("input", handleVolume)
+
+keysCheck.addEventListener("click", showHideKeys)
